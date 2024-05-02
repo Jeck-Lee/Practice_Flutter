37cd367df7983c58569160668e44518e9a0cb8f5
Jeck-Lee <jecklee02@gmail.com>
2024-05-02 10:22:24 +0900

    flutter_inappwebview git example 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,173 @@
-import 'dart:convert';
+import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:pointer_interceptor/pointer_interceptor.dart';
 
-void main() {
-  /// Webview 위젯 바인딩 초기화 - 웹뷰와 플러터 엔진과의 상호작용을 위함
+import 'chrome_safari_browser_example.screen.dart';
+import 'headless_in_app_webview.screen.dart';
+import 'in_app_webiew_example.screen.dart';
+import 'in_app_browser_example.screen.dart';
+import 'web_authentication_session_example.screen.dart';
+
+// import 'package:path_provider/path_provider.dart';
+// import 'package:permission_handler/permission_handler.dart';
+
+final localhostServer = InAppLocalhostServer(documentRoot: 'assets');
+
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // await Permission.camera.request();
+  // await Permission.microphone.request();
+  // await Permission.storage.request();
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  final String testCode = """
-  <!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container" style="height:100%;width:100%">
-  <div class="tradingview-widget-container__widget" style="height:calc(100% - 32px);width:100%"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
-  {
-  "autosize": true,
-  "symbol": "NASDAQ:AAPL",
-  "interval": "D",
-  "timezone": "Etc/UTC",
-  "theme": "light",
-  "style": "1",
-  "locale": "en",
-  "enable_publishing": false,
-  "allow_symbol_change": true,
-  "calendar": false,
-  "support_host": "https://www.tradingview.com"
-}
-  </script>
-</div>
-<!-- TradingView Widget END -->
-  """;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late InAppWebViewController webView;
-  bool status = false;
-
-  _changeUrl() {
-    webView.loadUrl(
-      urlRequest: URLRequest(
-        url: WebUri("https://flutter.dev/"),
-      ),
-    );
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
   }
 
-  Widget myPlayer(String url) {
-    debugPrint('----------WebView con: $url');
-    return InAppWebView(
-      key: GlobalKey(),
-      initialUrlRequest: URLRequest(
-        // url: WebUri("https://naver.com"),
-        url: WebUri.uri(
-          Uri.dataFromString(
-            widget.testCode,
-            mimeType: 'text/html',
-            encoding: Encoding.getByName('utf-8'),
+  if (!kIsWeb) {
+    await localhostServer.start();
+  }
+
+  runApp(MyApp());
+}
+
+PointerInterceptor myDrawer({required BuildContext context}) {
+  var children = [
+    ListTile(
+      title: Text('InAppWebView'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/');
+      },
+    ),
+    ListTile(
+      title: Text('InAppBrowser'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/InAppBrowser');
+      },
+    ),
+    ListTile(
+      title: Text('ChromeSafariBrowser'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/ChromeSafariBrowser');
+      },
+    ),
+    ListTile(
+      title: Text('WebAuthenticationSession'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/WebAuthenticationSession');
+      },
+    ),
+    ListTile(
+      title: Text('HeadlessInAppWebView'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/HeadlessInAppWebView');
+      },
+    ),
+  ];
+  if (kIsWeb) {
+    children = [
+      ListTile(
+        title: Text('InAppWebView'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/');
+        },
+      )
+    ];
+  } else if (defaultTargetPlatform == TargetPlatform.macOS) {
+    children = [
+      // ListTile(
+      //   title: Text('InAppWebView'),
+      //   onTap: () {
+      //     Navigator.pushReplacementNamed(context, '/');
+      //   },
+      // ),
+      // ListTile(
+      //   title: Text('InAppBrowser'),
+      //   onTap: () {
+      //     Navigator.pushReplacementNamed(context, '/InAppBrowser');
+      //   },
+      // ),
+      ListTile(
+        title: Text('InAppBrowser'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/');
+        },
+      ),
+      ListTile(
+        title: Text('WebAuthenticationSession'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/WebAuthenticationSession');
+        },
+      ),
+      ListTile(
+        title: Text('HeadlessInAppWebView'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/HeadlessInAppWebView');
+        },
+      ),
+    ];
+  }
+  return PointerInterceptor(
+    child: Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('flutter_inappwebview example'),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
           ),
-        ),
+          ...children
+        ],
       ),
-      initialSettings: InAppWebViewSettings(
-        javaScriptEnabled: true,
-        // 하이브리드 사용을 위한 안드로이드 웹뷰 최적화
-        useHybridComposition: true,
-      ),
-      onWebViewCreated: (InAppWebViewController controller) {
-        webView = controller;
-      },
-      onLoadStart: (controller, url) {
-        status = false;
-      },
-      onLoadStop: (controller, url) {
-        status = true;
-      },
-      onWindowFocus: (controller) => {},
-    );
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("TEST!"),
-      ),
-      body: myPlayer("https://pub.dev/"),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _changeUrl,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    if (kIsWeb) {
+      return MaterialApp(initialRoute: '/', routes: {
+        '/': (context) => InAppWebViewExampleScreen(),
+      });
+    }
+    if (defaultTargetPlatform == TargetPlatform.macOS) {
+      return MaterialApp(initialRoute: '/', routes: {
+        // '/': (context) => InAppWebViewExampleScreen(),
+        // '/InAppBrowser': (context) => InAppBrowserExampleScreen(),
+        '/': (context) => InAppBrowserExampleScreen(),
+        '/HeadlessInAppWebView': (context) =>
+            HeadlessInAppWebViewExampleScreen(),
+        '/WebAuthenticationSession': (context) =>
+            WebAuthenticationSessionExampleScreen(),
+      });
+    }
+    return MaterialApp(initialRoute: '/', routes: {
+      '/': (context) => InAppWebViewExampleScreen(),
+      '/InAppBrowser': (context) => InAppBrowserExampleScreen(),
+      '/ChromeSafariBrowser': (context) => ChromeSafariBrowserExampleScreen(),
+      '/HeadlessInAppWebView': (context) => HeadlessInAppWebViewExampleScreen(),
+      '/WebAuthenticationSession': (context) =>
+          WebAuthenticationSessionExampleScreen(),
+    });
   }
 }
