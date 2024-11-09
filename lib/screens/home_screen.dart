fb94cc0f3b73e46af5586aa21193170a08f202de
Jeck-Lee <jecklee02@gmail.com>
2024-11-09 12:10:29 +0900

    ✨ Add navigation methods to Detail screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,6 +23,26 @@ class HomeScreen extends StatelessWidget {
                 "push /content",
               ),
             ),
+            const SizedBox(height: 36),
+
+            /// 1. 루트에서 /content/detail 경로로 이동하게 되면 content 화면과 detail 화면이 둘 다 추가될까?
+            ElevatedButton(
+              onPressed: () {
+                context.push("/content/detail");
+              },
+              child: const Text(
+                "push /content/detail",
+              ),
+            ),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: () {
+                context.go("/content/detail");
+              },
+              child: const Text(
+                "go /content/detail",
+              ),
+            ),
           ],
         ),
       ),
